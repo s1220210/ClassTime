40795b6fecb530e268754417feaab9c279486f21
s1220210 <s1220210@gmail.com>
2018-02-24 02:25:46 +0900

    a

---
+++ ClassTime/ScheduleViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ScheduleViewController.swift
+//  ClassTime
+//
+//  Created by owner on 2018/02/24.
+//  Copyright © 2018年 owner. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ScheduleViewController: UIViewController {
+
+    @IBOutlet weak var classNumber: UITextField!
+    
+    var myClass = Classes()
+    let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Realmに保存されてるDog型のオブジェクトを全て取得
+        let classNumbers = realm.objects(Classes.self)
+        // 先頭の犬を取り出し
+        if let cn = classNumbers.first {
+            // データを更新
+            let str: String = String(cn.classNumber)
+            classNumber.text = str
+            // write()に渡すブロックの外だと例外発生
+            //      dog.name = "First"
+        }
+        else{
+            let str: String = classNumber.text!
+            myClass.classNumber = Int(str)!
+            try! realm.write() {
+                realm.add(myClass)
+            }
+        }
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("DEBUG: viewWillDisappear")
+        // Realmに保存されてるDog型のオブジェクトを全て取得
+        let classNumbers = realm.objects(Classes.self)
+        // 先頭の犬を取り出し
+        if let cn = classNumbers.first {
+            // データを更新
+            try! realm.write() {
+                let str: String = classNumber.text!
+                cn.classNumber = Int(str)!
+            }
+        }
+        super.viewWillDisappear(animated)
+        print("DEBUG: viewWillDisappear_")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ClassTime/Week.swift
@@ -0,0 +1,32 @@
+//
+//  Week.swift
+//  ClassTime
+//
+//  Created by owner on 2018/02/24.
+//  Copyright © 2018年 owner. All rights reserved.
+//
+
+
+import RealmSwift
+
+class Week: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    // week
+    @objc dynamic var w0 = 0
+    @objc dynamic var w1 = 0
+    @objc dynamic var w2 = 0
+    @objc dynamic var w3 = 0
+    @objc dynamic var w4 = 0
+    @objc dynamic var w5 = 0
+    @objc dynamic var w6 = 0
+    //時限数
+    @objc dynamic var classNumber = 0
+}
+
+class Classes: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    //時限数
+    @objc dynamic var classNumber = 0
+}
