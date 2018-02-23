0ebb8924cbec177c43c6ae66886dec8aecdbd1a9
s1220210 <s1220210@gmail.com>
2018-02-24 01:02:40 +0900

    Initial Commit

---
+++ ClassTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassTime
+//
+//  Created by owner on 2018/02/24.
+//  Copyright © 2018年 owner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
